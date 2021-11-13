270c563e0c0d12e3d8f32b792934bf5901c555f3
rmac94 <ronan.machin@yahoo.co.uk>
2021-11-13 16:46:19 +0000

    setup py

--- stocktracker/__init__.py
+++ stocktracker/__init__.py
@@ -1,5 +1,5 @@
-import misc
+# import misc
 print("Successfully imported stock-tracker")
 
-if not misc.credentials_check():
-    print("No API credentials found, add credentials.ini to project folder!")
+# if not misc.credentials_check():
+#     print("No API credentials found, add credentials.ini to project folder!")
