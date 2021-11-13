b5e734a386b7ec553ac154d8a67bd8e23961ad81
rmac94 <ronan.machin@yahoo.co.uk>
2021-11-13 16:44:40 +0000

    setup py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='stock-tracker',
-      version='1.0.2',
+      version='1.0.3',
       description='Package to extract stock information',
       author='Ronan Machin',
       packages=['stocktracker'],
