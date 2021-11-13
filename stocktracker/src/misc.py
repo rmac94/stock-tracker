7270e8d9b3e650c19d5827f7857b303e2d7cf8d4
rmac94 <ronan.machin@yahoo.co.uk>
2021-11-13 15:45:55 +0000

    first attempt at packaging

--- stocktracker/src/misc.py
+++ stocktracker/src/misc.py
@@ -1,5 +1,6 @@
 from requests import get
 import os, pathlib, glob, socket
+import configparser
 
 def cloud_provider():
     if "microsoft" in get('https://ipinfo.io').json()['org'].lower():
@@ -11,14 +12,18 @@ def project_path():
     return str(pathlib.Path(os.path.abspath('')).parent)
 
 def credentials_check():
-    files = glob.glob(str(pathlib.Path(project_path().parent))+'/*')
+    files = glob.glob(str(pathlib.Path(project_path()).parent)+'/*')
     return [True for x in files if 'credentials' in x][0]
     
 
 def read_credentials():
-    cred_path = os.path.join(str(pathlib.Path(os.path.abspath('')).parent.parent), 'credentials.ini')
-    if socket.gethostname() == 'Test3':
-        cred_path = os.path.join(str(pathlib.Path(os.path.abspath(''))), 'credentials.ini')
+    
+    cred_path = str(pathlib.Path(project_path()).parent) +'/credentials.ini'
+    config = configparser.ConfigParser()
+    config.read(cred_path)
+    return config
+    
+
 
 
 if __name__ == "__main__":
